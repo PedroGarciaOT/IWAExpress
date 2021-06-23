d19a9923e0166672c4c05d9dbe96eaee727f6fc9
akevinlee <kevin.lee@microfocus.com>
2021-06-23 13:47:24 +0100

    Updated SSL init

--- bin/init-letsencrypt.sh
+++ bin/init-letsencrypt.sh
@@ -5,11 +5,11 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(nodeiwa.mfdemouk.com)
+domains=(iwaexpress.mfdemouk.com)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="" # Adding a valid address is strongly recommended
-staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   echo "WARNING: Existing data found for $domains. Replacing... "
